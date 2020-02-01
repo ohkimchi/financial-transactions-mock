5183a2aac45def790cd3b0c415d2e35cd5e41453
max <maxjunhuixu@gmail.com>
2020-02-01 10:36:04 +0800

    Updates

---
+++ precache-manifest.6218bf183e8bb6edcd4a538b89d6b102.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "372bbd4b1452bd38ad5823acfd0243d9",
+    "url": "/financial-transactions-mock/index.html"
+  },
+  {
+    "revision": "287647aff74baad1139c",
+    "url": "/financial-transactions-mock/static/css/main.b4a3080c.chunk.css"
+  },
+  {
+    "revision": "c91b0e7a09703a07e09c",
+    "url": "/financial-transactions-mock/static/js/2.a185e9f5.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/financial-transactions-mock/static/js/2.a185e9f5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "287647aff74baad1139c",
+    "url": "/financial-transactions-mock/static/js/main.085e3b60.chunk.js"
+  },
+  {
+    "revision": "73dece5090a55e20aead",
+    "url": "/financial-transactions-mock/static/js/runtime-main.ac7c3b86.js"
+  }
+]);
